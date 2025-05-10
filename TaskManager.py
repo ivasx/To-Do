1ca2b6b16a57f8a49bb91ab7ee025d38360c9c75
ivasx <ambroziak.v.ivan@gmil.com>
2025-05-10 11:50:22 +0300

    Documentation in TaskManager.py

--- TaskManager.py
+++ TaskManager.py
@@ -6,6 +6,16 @@ class TaskManager:
         self.__lists = {}
 
     def create_list(self, list_name):
+        """
+        Створює новий список завдань з вказаною назвою.
+
+        Якщо список з такою назвою вже існує — виводиться відповідне повідомлення.
+
+        Параметри:
+        list_name : str
+            Назва списку, який потрібно створити.
+        """
+
         if list_name in self.__lists:
             print(f"Список '{list_name}' вже існує.")
         else:
@@ -16,6 +26,16 @@ class TaskManager:
         return self.__lists.get(list_name, None)
 
     def delete_list(self, list_name):
+        """
+        Видаляє список завдань за вказаною назвою.
+
+        Якщо список не існує — виводиться повідомлення про помилку.
+
+        Параметри:
+        list_name : str
+            Назва списку, який потрібно видалити.
+        """
+
         if list_name in self.__lists:
             del self.__lists[list_name]
             print(f"Список '{list_name}' видалено.")
